b394eef0f037e40b7f421923bbef3fd73f71662b
Ochan George <gwochan24@gmail.com>
2021-02-07 01:06:06 +0300

    final reponsiveness edit for this day

--- javascript/partialInclude.js
+++ javascript/partialInclude.js
@@ -27,3 +27,21 @@ function includeHTML() {
   }
 
   includeHTML();
+
+ 
+backTopButton = document.getElementById("backbutton");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    backTopButton.style.display = "block";
+  } else {
+    backTopButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}

5d691a2f941c9bf50573d3053ea86fb4c7dbe035
Ochan George <gwochan24@gmail.com>
2021-03-28 13:52:47 +0300

    middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // Route files
 const topics = require('./routes/topics');
@@ -8,6 +9,11 @@ const topics = require('./routes/topics');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/', topics);
